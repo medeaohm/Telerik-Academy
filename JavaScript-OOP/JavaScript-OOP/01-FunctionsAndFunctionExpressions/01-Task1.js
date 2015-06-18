7cc59ce5b3a21f61287501d7427f3509a8811f14
medeaohm <nia_omerska@abv.bg>
2015-06-18 21:43:13 +0200

    JavaScript-OOP added to GitHub

---
+++ JavaScript-OOP/JavaScript-OOP/01-FunctionsAndFunctionExpressions/01-Task1.js
@@ -0,0 +1,32 @@
+﻿/*
+ * Write a function that sums an array of numbers:
+ * Numbers must be always of type Number
+ * Returns null if the array is empty
+ * Throws Error if the parameter is not passed (undefined)
+ * Throws if any of the elements is not convertible to Number
+ */
+
+function solve(arr) {
+    if (arr == undefined) {
+        throw 'Function argument missing'
+    }
+    else if (!arr.length) {
+        return null;
+    }
+    else {
+        var sum = 0,
+        i = 0,
+        length = arr.length;
+
+        for (i; i < length; i += 1) {
+            if (isNaN(parseInt(arr[i]))) {
+                throw 'Function argument is not a number'
+            }
+            sum += parseInt(arr[i]);
+        }
+        return sum;
+    }
+}
+
+var test = [1, 2, 3, 4];
+console.log(solve(test));
---
+++ JavaScript-OOP/JavaScript-OOP/01-FunctionsAndFunctionExpressions/01-Task2.js
@@ -0,0 +1,45 @@
+﻿/*
+ * Write a function that finds all the prime numbers in a range
+ * It should return the prime numbers in an array
+ * It must throw an Error if any of the range params is not convertible to Number
+ * It must throw an Error if any of the range params is missing
+ */
+
+function primeNumbers(start, end) {
+    if (start === undefined || end === undefined) {
+        throw new Error();
+    }
+    else if (isNaN(parseInt(start)) || isNaN(parseInt(end))) {
+        throw new Error();
+    }
+    else {
+        var primes = [],
+            n,
+            maxDivisor,
+            isPrime,
+            i,
+            start = +start,
+            end = +end;
+
+        for (n = start; n <= end; n += 1) {
+
+            maxDivisor = Math.sqrt(n);
+            isPrime = true;
+
+            for ( i = 2; i <= maxDivisor; i++) {
+                if (!(n % i)) {
+                    isPrime = false;
+                }
+            }
+            if (n === 1 || n === 0) {
+                isPrime = false;
+            }
+            if (isPrime) {
+                primes.push(n);
+            }
+        }
+    }
+    return primes;
+}
+
+console.log(primeNumbers(0, 6));
