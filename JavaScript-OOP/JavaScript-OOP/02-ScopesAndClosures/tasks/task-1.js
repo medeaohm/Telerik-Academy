83bbe4a93ac2e07ca6e8845033f49d574deff345
medeaohm <nia_omerska@abv.bg>
2015-06-21 18:49:50 +0200

    ...

--- JavaScript-OOP/JavaScript-OOP/02-ScopesAndClosures/tasks/task-1.js
+++ JavaScript-OOP/JavaScript-OOP/02-ScopesAndClosures/tasks/task-1.js
@@ -22,20 +22,113 @@
 */
 function solve() {
 	var library = (function () {
-		var books = [];
-		var categories = [];
-		function listBooks() {
-			return books;
+	    var books = [];
+	    var categories = [];
+
+		function listBooks(currentBook) {
+		    if (arguments.length > 0) {
+		        if (typeof currentBook.category !== 'undefined') {
+		            if (typeof categories[currentBook.category] !== 'undefined') {
+		                return categories[currentBook.category].books;
+		            } else {
+		                return [];
+		            }
+		        }
+		        if (typeof currentBook.author !== 'undefined') {
+		            var booksInList = [];
+		            for (var i = 0; i < books.length; i++) {
+		                if (books[i].author === currentBook.author) {
+		                    booksInList.push(books[i]);
+		                }
+		            }
+		            return booksInList;
+		        }
+		    }
+		    return books;
+		}
+
+
+		function alreadyExist(name, type) {
+		    for (var i = 0; i < books.length; i++) {
+		        if (books[i][type] === name) {
+		            return true;
+		        }
+		    }
+		    return false;
+		}
+
+		function createCategory(name) {
+		    categories[name] = {
+		        books: [],
+		        ID: categories.length + 1
+		    };
+		}
+
+		function checkBookInfo(book, argumentLenght) {
+		    if (Object.keys(book).length !== argumentLenght) {
+		        throw new Error('Book parameters missing');
+		    }
+		    for (var parameter in book) {
+		        if (typeof book[parameter] === 'undefined') {
+		            throw new Error(parameter + ' is undefined');
+		        }
+		    }
+		}
+
+		function validAuthor(author) {
+		    if (author.trim() === '') {
+		        throw new Error('Author name cannot be empty');
+		    }
+		}
+
+		function validISBN(isbn) {
+		    if (isbn.length !== 10 && isbn.length !== 13) {
+		        throw new Error('ISBN must coinatin either 10 or 13 digits');
+		    }
+		    if (!/^[0-9]+$/.test(isbn.toString())) {
+		        throw new Error('ISBN must contains only numbers');
+		    }
+		}
+
+		function validationLenght(name) {
+		    if (name.length < 2 || name.length > 100) {
+		        throw new Error('Field should be more than 2 and less than 100 symbols');
+		    }
 		}
 
 		function addBook(book) {
-			book.ID = books.length + 1;
-			books.push(book);
-			return book;
+		    book.ID = books.length + 1;
+
+		    checkBookInfo(book, 5);
+
+		    if (alreadyExist(book.title, 'title')) {
+		        throw new Error('A book with the same title already exists.');
+		    }
+		    if (alreadyExist(book.isbn, 'isbn')) {
+		        throw new Error('A book with the same ISBN already exists.');
+		    }
+		    if (categories.indexOf(book.category) < 0) {
+		        createCategory(book.category);
+		    }
+
+		    validAuthor(book.author);
+		    validISBN(book.isbn);
+		    validationLenght(book.title);
+		    validationLenght(book.category);
+
+		    categories[book.category].books.push(book);
+
+		    books.push(book);
+		    return book;
 		}
 
-		function listCategories() {
-			return categories;
+
+		function listCategories(category) {
+		    var categoriesName = [];
+
+		    Array.prototype.push.apply(categoriesName, Object.keys(categories));
+
+		    return categoriesName;
 		}
 
 		return {
--- JavaScript-OOP/JavaScript-OOP/03-ClassicalInheritance/tasks/task-1.js
+++ JavaScript-OOP/JavaScript-OOP/03-ClassicalInheritance/tasks/task-1.js
@@ -6,7 +6,7 @@
 		*	age must always be a number in the range 0 150
 			*	the setter of age can receive a convertible-to-number value
 		*	if any of the above is not met, throw Error 		
-	*	property `fullname`
+	*	property `fullastname`
 		*	the getter returns a string in the format 'FIRST_NAME LAST_NAME'
 		*	the setter receives a string is the format 'FIRST_NAME LAST_NAME'
 			*	it must parse it and set `firstname` and `lastname`
@@ -17,11 +17,84 @@
 */
 function solve() {
 	var Person = (function () {
-		function Person() {
-		}
-		
+	    function Person(firstname, lastname, age) {
+	        this.firstname = firstname;
+	        this.lastname = lastname;
+	        this.age = age;
+	    }
+
+	    Object.defineProperty(Person.prototype, 'firstname', {
+	        get: function () {
+	            return this._firstname;
+	        },
+	        set: function (first) {
+	            if (!validateName(first)) {
+	                throw new Error('Firstname is invalid');
+	            }
+	            this._firstname = first;
+	        }
+	    });
+
+	    Object.defineProperty(Person.prototype, 'lastname', {
+	        get: function () {
+	            return this._lastname;
+	        },
+	        set: function (last) {
+	            if (!validateName(last)) {
+	                throw new Error('Lastname is invalid');
+	            }
+	            this._lastname = last;
+	        }
+	    });
+
+	    Object.defineProperty(Person.prototype, 'fullname', {
+	        get: function () {
+	            return this.firstname + ' ' + this.lastname;
+	        },
+	        set: function (full) {
+	            var names = full.split(' ');
+	            this.firstname = names[0];
+	            this.lastname = names[1];
+	        }
+	    });
+
+	    Object.defineProperty(Person.prototype, 'age', {
+	        get: function () {
+	            return this._age;
+	        },
+	        set: function (value) {
+	            if (!validateAge(value)) {
+	                throw new Error('Age is invalid');
+	            }
+	            this._age = value;
+	        }
+	    });
+        
+	    Person.prototype.introduce = function () {
+	        return 'Hello! My name is ' + this.fullname + ' and I am ' + this.age + '-years-old';
+	    }
+
+	    function validateName(name) {
+	        if (/^[A-Za-z]{3,20}$/.test(name)) {
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    function validateAge(age) {
+	        if (!isNaN(parseInt(age))) {
+	            if (age > 0 && age < 150) {
+	                return true;
+	            }
+	            return false;
+	        }
+	        return false;
+	    }
+
 		return Person;
 	} ());
 	return Person;
 }
+
+
 module.exports = solve;
