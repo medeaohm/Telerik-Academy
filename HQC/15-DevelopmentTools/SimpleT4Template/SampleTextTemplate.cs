b04278b9b635a9ad515d68d251111b58f0d8d792
medeaohm <nia_omerska@abv.bg>
2015-08-19 21:21:32 +0300

    Added development Tools Homework

---
+++ HQC/15-DevelopmentTools/SimpleT4Template/SampleTextTemplate.cs
@@ -0,0 +1,12 @@
+﻿
+public class File
+{
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public string Extenstion { get; set; }
+
+    public int Dimension { get; set; }
+
+}
