19d532b1a1e82a5da64c9cf6e83b7d0ddbcb85a8
medeaohm <nia_omerska@abv.bg>
2015-11-15 12:06:16 +0200

    third workshop

---
+++ DSA/workshops/3-Search-And-Sort-Algorithms-And-Combinatorics/Workshop3/BinaryTree/BinaryTreeStartup.cs
@@ -0,0 +1,64 @@
+﻿namespace BinaryTree
+{
+    using System;
+    using System.Numerics;
+
+    public class BinaryTreeStartup
+    {
+        static long[] memo;
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            var groups = new int[26];
+
+            foreach (var ball in input)
+            {
+                groups[ball - 'A']++;
+            }
+
+            int numberOfBalls = input.Length;
+
+            memo = new long[numberOfBalls + 1];
+            var factorials = new long[numberOfBalls + 1];
+            factorials[0] = 1;
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                factorials[i + 1] = factorials[i] * (i + 1);
+            }
+
+            long result = factorials[numberOfBalls];
+            foreach (var item in groups)
+            {
+                result /= factorials[item];
+            }
+
+            BigInteger numberOfColouredTrees = result;
+            numberOfColouredTrees *= NumberOfTrees(numberOfBalls);
+            Console.WriteLine(numberOfColouredTrees);
+        }
+
+        static long NumberOfTrees(int numberOfBalls)
+        {
+            if (numberOfBalls == 0)
+            {
+                return 1;
+            }
+
+            if (memo[numberOfBalls] > 0)
+            {
+                return memo[numberOfBalls];
+            }
+
+            long result = 0;
+            for (int i = 0; i < numberOfBalls; i++)
+            {
+                result += NumberOfTrees(i) * NumberOfTrees(numberOfBalls - 1 - i);
+            }
+
+            memo[numberOfBalls] = result;
+            return result;
+        }
+    }
+}
+
---
+++ DSA/workshops/3-Search-And-Sort-Algorithms-And-Combinatorics/Workshop3/Precision/PrecisionStartup.cs
@@ -0,0 +1,92 @@
+﻿namespace Precision
+{
+    using System;
+
+    public class PrecisionStartup
+    {
+        public static void Main()
+        {
+            int maxDenominator = int.Parse(Console.ReadLine());
+            var fraction = Console.ReadLine().Split('.')[1];
+
+            int bestNom = 0;
+            int bestDen = 1;
+            int bestPrecision = 0;
+            for (int den = 1; den <= maxDenominator; den++)
+            {
+                int left = 0;
+                int right = den;
+
+                while (left < right)
+                {
+                    int middle = (left + right) / 2;
+                    if (Compare(fraction, middle, den))
+                    {
+                        right = middle;
+                    }
+                    else
+                    {
+                        left = middle + 1;
+                    }
+                }
+
+                int currentPrecision = Precision(fraction, left - 1, den);
+                if (currentPrecision > bestPrecision)
+                {
+                    bestDen = den;
+                    bestNom = left - 1;
+                    bestPrecision = currentPrecision;
+                }
+
+                currentPrecision = Precision(fraction, left, den);
+                if (currentPrecision > bestPrecision)
+                {
+                    bestDen = den;
+                    bestNom = left;
+                    bestPrecision = currentPrecision;
+                }
+            }
+
+            Console.WriteLine("{0}/{1}", bestNom, bestDen);
+            Console.WriteLine(bestPrecision + 1);  
+        }
+
+        static int Precision(string fraction, int a, int b)
+        {
+            a *= 10;
+            int i;
+
+            for (i = 0; i < fraction.Length; i++)
+            {             
+                int digit = a / b;
+                if (digit != fraction[i] - '0')
+                {
+                    break;
+                }
+                a = a % b * 10;
+            }
+            return i;
+        }
+
+        static bool Compare(string fraction, int a, int b)
+        {
+            a *= 10;
+            int i;
+
+            for (i = 0; i < fraction.Length; i++)
+            {
+                int digit = a / b;
+                if (digit < fraction[i] - '0')
+                {
+                    return false;
+                }
+                else if (digit > fraction[i] - '0')
+                {
+                    return true;
+                }
+                a = a % b * 10;
+            }
+            return true;
+        }
+    }
+}
