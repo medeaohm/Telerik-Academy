4f4df6ea6624a34cfb6d3b6ce7172fb076d2d897
medeaohm <nia_omerska@abv.bg>
2016-02-22 11:52:49 +0200

    Register Form UI fixed

--- ASP.NET - MVC/IndividualProject/LostPets/Source/Web/LostPets.Web/ViewModels/Account/RegisterViewModel.cs
+++ ASP.NET - MVC/IndividualProject/LostPets/Source/Web/LostPets.Web/ViewModels/Account/RegisterViewModel.cs
@@ -10,19 +10,23 @@
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
+        [UIHint("RegisterSingleLineText")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 4)]
         [Display(Name = "Username")]
+        [UIHint("RegisterSingleLineText")]
         public string UserName { get; set; }
 
         [StringLength(30)]
         [Display(Name = "First name")]
+        [UIHint("RegisterSingleLineText")]
         public string FirstName { get; set; }
 
         [StringLength(30)]
         [Display(Name = "Last name")]
+        [UIHint("RegisterSingleLineText")]
         public string LastName { get; set; }
 
         [Display(Name = "Gender")]
@@ -35,21 +39,25 @@
 
         [RegularExpression(@"^\d{10}$", ErrorMessage = "The phone number must be exactly 10 digits. ")]
         [Display(Name = "Phone Number")]
+        [UIHint("RegisterSingleLineText")]
         public string PhoneNumber { get; set; }
 
         [StringLength(200)]
         [Display(Name = "Facebook Profile")]
+        [UIHint("RegisterSingleLineText")]
         public string FacebookProfile { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [UIHint("RegisterPassword")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [UIHint("RegisterPassword")]
         public string ConfirmPassword { get; set; }
     }
 }
